6bbf8e388aa1905db9e4efeca117ccac84c8cc82
RayAlvMar <24308060610590@cetis61.edu.mx>
2026-05-18 12:30:25 -0600

    Correo

--- main.py
+++ main.py
@@ -12,6 +12,41 @@ client = MongoClient("mongodb+srv://Raytest:raysito123@ralex.lbaspzb.mongodb.net
 db = client["Tienda_Perritos"]
 usuarios_collection = db["usuarios"]
 
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL_SENDER = "perritostienda61@gmail.com"
+EMAIL_PASSWORD = "njzd jlaa gwlr mqvs"
+
+
+def enviar_correo(destinatario, token):
+    link = f"http://127.0.0.1:5000/reset/{token}"
+
+    mensaje = MIMEText(f"""
+Hola.
+
+Haz clic en el siguiente enlace para restablecer tu contraseña:
+
+{link}
+
+Si no solicitaste esto, ignora este correo.
+""")
+
+    mensaje["Subject"] = "Recuperación de contraseña"
+    mensaje["From"] = EMAIL_SENDER
+    mensaje["To"] = destinatario
+
+    servidor = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    servidor.starttls()
+    servidor.login(EMAIL_SENDER, EMAIL_PASSWORD)
+
+    servidor.sendmail(
+        EMAIL_SENDER,
+        destinatario,
+        mensaje.as_string()
+    )
+
+    servidor.quit()
+
 @app.route('/')
 def home():
     return render_template('login.html')
@@ -65,29 +100,55 @@ def registrar():
         session["usuario"] = email
         session["nombre"] = nombre
         flash("Registro exitoso", "success")
-        return redirect("/base")    
+        return redirect("/login")    
     return render_template("registro.html")
 
-@app.route("/recuperar", methods=["POST"])
+@app.route("/recuperar", methods=["GET", "POST"])
 def recuperar():
+
+    if request.method == "GET":
+        return render_template("recuperar.html")
+
     email = request.form.get("email")
 
     usuario = usuarios_collection.find_one({"email": email})
 
     if usuario:
-        token = secrets.token_urlsafe(32)  # 👈 generas token
+        token = secrets.token_urlsafe(32)
 
-        usuarios_collection.update_one(   # 👈 AQUÍ VA
+        usuarios_collection.update_one(
             {"email": email},
-            {"$set": {
-                "reset_token": token
-            }}
+            {"$set": {"reset_token": token}}
         )
 
-        enviar_correo(email, token)  # 👈 mandas el correo
+        enviar_correo(email, token)
 
     flash("Si el correo existe, se envió un enlace", "info")
     return redirect("/login")
+
+@app.route("/reset/<token>", methods=["GET", "POST"])
+def reset(token):
+    user = usuarios_collection.find_one({"reset_token": token})
+
+    if not user:
+        return "Token inválido"
+
+    if request.method == "POST":
+        nueva_password = request.form["password"]
+
+        password_hash = generate_password_hash(nueva_password)
+
+        usuarios_collection.update_one(
+            {"reset_token": token},
+            {
+                "$set": {"password": password_hash},
+                "$unset": {"reset_token": ""}
+            }
+        )
+
+        return "Contraseña actualizada"
+
+    return render_template("reset.html")
 
 @app.route("/base")
 def base():
